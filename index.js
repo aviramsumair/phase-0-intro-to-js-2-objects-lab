2d2815a9d9c8395d229737a3e1e9dc34d5cbfe50
Avinash Ramsumair <avi@pure235games.com>
2022-11-12 16:23:08 -0500

    commit tree 642e57269675e3809d4fa64e6df4177122d47bf0

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+const employee = {
+    name: "newName",
+    streetAddress: "newAddress"
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value,){
+    return{
+        ...employee,
+        [key]: value 
+    };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key] = value;
+    return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key){
+    const cloneEmpl = {...employee}
+    delete cloneEmpl[key];
+    return cloneEmpl;    
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key){
+    delete obj[key];
+    return obj;
+}
